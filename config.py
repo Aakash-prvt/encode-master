e60ad084bf67c123d4580c139b7a48978a44e7bf
Aakash-prvt <akashpremiums03@gmail.com>
2024-08-21 19:30:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
    
 class Config(object):
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 5725206423))
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://compressor:compressor@cluster0.cqo0lqp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001900161200"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
 
